b8d2887478b78559d3fda2f3f463b6a94fde55b4
Christoffer Nordvall <spjutbjorn@gmail.com>
2016-04-28 19:02:24 +0200

    person class

--- project/person.ts
+++ project/person.ts
@@ -1,5 +1,20 @@
-﻿class Person {
-    
+﻿interface IPerson {
+    name: ()=> string;
+    firstPerson: string;
+    lastPerson: string;
+}
+
+class Person implements IPerson {
+    firstName: string;
+    lastName: string;
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    name() {
+        return this.firstName + " " + this.lastName;
+    }
+     
 }
 
 external = Person;
